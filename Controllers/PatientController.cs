21b2340d414ef33115377314d9f478058edcd4e5
Darya Gahramanova <darya1@usf.edu>
2020-04-10 12:17:37 -0400

    Add project files.

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PatientModelBinding.Models;
+
+namespace PatientModelBinding.Controllers
+{
+    public class PatientController : Controller
+    {
+        public ViewResult PatientInfo()
+        {
+            Patient patient = new Patient();
+            return View(patient);
+        }
+        [HttpPost]
+        public ViewResult PatientInfo(Patient patient)
+        {
+            return View(patient);
+        }
+    }
+}
---
+++ Models/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientModelBinding.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsDischarged { get; set; }
+    }
+}
